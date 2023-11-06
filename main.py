b3f2e5fb557fa750123a5aaaee0850a535da98d6
HyodaKazuaki <kazuaki@ex-t.jp>
2023-11-06 09:00:30 +0000

    fix memo

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ parser.add_argument("--log_step", default=100, type=int)
 parser.add_argument("--sty_dim", default=128, type=int, help="The size of style vector")
 parser.add_argument(
     "--output_k", default=400, type=int, help="Total number of classes to use"
-)  # It means the number of characters
+)  # It means the number of fonts
 parser.add_argument("--img_size", default=80, type=int, help="Input image size")
 parser.add_argument("--dims", default=2048, type=int, help="Inception dims for FID")
 
